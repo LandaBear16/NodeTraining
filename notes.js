38c76a468952fd202f5f2294cbbc750c0e6235a6
Yolanda <landa.collins14@gmail.com>
2019-01-28 20:17:49 +0000

    writing to file

--- notes.js
+++ notes.js
@@ -1,7 +1,25 @@
 console.log("Starting notes.js");
 
+const fs = require("fs");
+
 const addNote = (title, body) => {
-  console.log("adding note", title, body);
+  var notes = [];
+  var note = {
+    title,
+    body
+  };
+
+  try {
+    var noteString = fs.readFileSync("notes-data.json");
+    notes = JSON.parse(noteString);
+  } catch (e) {}
+
+  var duplicateNotes = notes.filter(note => note.title === title);
+
+  if (duplicateNotes.length === 0) {
+    notes.push(note);
+    fs.writeFileSync("notes-data.json", JSON.stringify(notes));
+  }
 };
 
 const getAll = () => {
